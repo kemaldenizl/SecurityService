f60584c88b14985ba333d0656b29508f036cf03b
kemaldenizl <kemaldenizbum@gmail.com>
2026-07-12 23:27:06 +0300

    refactor(tenant-management): removing tenant management commands and endpoints

--- Security.Application/Tenants/GetTenants/GetTenantsQuery.cs
+++
@@ -1,7 +0,0 @@
-using MediatR;
-using Security.Application.Common.Results;
-using Security.Application.Tenants.Dtos;
-
-namespace Security.Application.Tenants.GetTenants;
-
-public sealed record GetTenantsQuery : IRequest<Result<IReadOnlyCollection<TenantDto>>>;
